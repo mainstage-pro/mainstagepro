7d2d1209938726b2042a2520eed0d4999bf2ee1e
Mauricio Hernández <mainstageqro@gmail.com>
2026-07-08 09:06:15 -0600

    chore: remove scratch files

--- extract.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/app/(dashboard)/crm/tratos/[id]/page.tsx', 'utf-8');
-
-// I will extract the file line by line but that might be complex.
-// Let's just create a basic version of DiscoveryForm that uses a fetch to /api/tratos/[id] internally
